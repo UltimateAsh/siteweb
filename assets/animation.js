58b4da5f16f288af0789870b3e9c5650fc4f0ec6
Achraf SOBHI <achrafsobhi.123@gmail.com>
2024-05-01 15:39:27 +0200

    assets

---
+++ assets/animation.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', { 
+    strings: ['Frontend Developer', 'un étudiant.', 'un ingénieur informatique à en devenir.', 'votre futur.'],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true
+    });
